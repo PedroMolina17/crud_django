ff72786352de2c7f61bd4d7cbd864e4d804288ab
Pedro Molina <pedro.molinanoa@gmail.com>
2023-12-10 03:11:28 -0500

    Login

--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
+from django.contrib.auth import login
+from django.db import IntegrityError
 
 
 def home(request):
@@ -9,22 +11,21 @@ def home(request):
 
 
 def signup(request):
+
     if request.method == 'GET':
-        return render(request, 'signup.html',
-                      {'form': UserCreationForm})
+        return render(request, 'signup.html',  {'form': UserCreationForm})
     else:
         if request.POST['password1'] == request.POST['password2']:
-            print(request.POST)
-            # register user
             try:
-                user = User.objects.create_user(username=request.POST['username'],
-                                                password=request.POST['password1'])
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
                 user.save()
+                login(request, user)
                 return redirect('tasks')
-            except:
+            except IntegrityError:
                 return render(request, 'signup.html',
                               {'form': UserCreationForm,
-                               'error': 'Username  already exist'})
+                               'error': 'User name already exists'})
         else:
             return render(request, 'signup.html',
                           {'form': UserCreationForm,
