2f3dbf39100c581448779d3aa465cab1cf68eee2
Pedro Molina <pedro.molinanoa@gmail.com>
2023-12-09 22:27:59 -0500

    git remote add origin https://github.com/PedroMolina17/crud_django.git   git branch -M main   git push -u origin main

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from  django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 def home(request):
      return render(request,'home.html')
@@ -10,7 +11,9 @@ def signup(request):
         return render(request,'signup.html',
           {'form': UserCreationForm})
     else:
-        if request.POST['password1'] == request.POST['password2']:
+        if request.POST['password1'] == request.POST['password1']:
             #register user 
-         print (request.POST)
-        print ('obteniendo datos')
+            user = User.objects.create_user(username=request.POST['username'],password=request.POST['password1'])
+            user.save()
+            return HttpResponse('User  registration successful')
+        return HttpResponse('Passwords do not match')
